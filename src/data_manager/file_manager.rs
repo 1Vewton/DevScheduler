0b53e3a2dcaa83e41bce2f84c170878fd2e5f9e9
NewtonZhan <zhanyunze0601@gmail.com>
2026-07-07 12:40:25 +0800

    feat(Projects): Add the method for projects that can get the random weight of each project

--- src/data_manager/file_manager.rs
+++ src/data_manager/file_manager.rs
@@ -1,2 +1,1 @@
-pub mod file_manager;
 use serde_json;
--- src/data_structure/projects.rs
+++ src/data_structure/projects.rs
@@ -1,12 +1,28 @@
-pub mod projects;
-
 // Project stores the data structure describing a project
 struct Project {
     project_name: String,
-    weight: Int,
+    weight: i64,
 }
 
 // Projets stores the data strucutre describing list of projects
 struct Projects {
-    list_of_projects: Vec<String>
+    list_of_projects: Vec<Project>
+}
+
+impl Projects {
+    // get_random_weight gets the random weight of each project
+    fn get_random_weight(&self) -> Vec<f64>{
+        let mut all_weights: i64 = 0;
+        for n in &self.list_of_projects{
+            all_weights += n.weight
+        }
+        let f_all_weights: f64 = all_weights as f64;
+        let sub_weights: f64 = 1.0/f_all_weights;
+        let mut result: Vec<f64> = Vec::new();
+        for n in &self.list_of_projects{
+            let n_weight: f64 = n.weight as f64;
+            result.push(n_weight*sub_weights)
+        }
+        return result
+    }
 }
